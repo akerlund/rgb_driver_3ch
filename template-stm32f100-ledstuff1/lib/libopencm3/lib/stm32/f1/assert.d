99a6472cead2798a558a2edfb49982a65eb8a203
akerlund <fredrik.akerlund.lh@gmail.com>
2019-12-26 17:29:52 +0100

    Remove compiled files

--- template-stm32f100-ledstuff1/lib/libopencm3/lib/stm32/f1/assert.d
+++
@@ -1,9 +0,0 @@
-assert.o: ../../cm3/assert.c ../../../include/libopencm3/cm3/assert.h \
- ../../../include/libopencm3/cm3/common.h \
- /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
- /usr/arm-none-eabi/include/stdint.h \
- /usr/arm-none-eabi/include/machine/_default_types.h \
- /usr/arm-none-eabi/include/sys/features.h \
- /usr/arm-none-eabi/include/sys/_intsup.h \
- /usr/arm-none-eabi/include/sys/_stdint.h \
- /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h
